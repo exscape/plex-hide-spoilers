0df50149c3b95d3cb9d8f1401308a8eaa06752f9
Thomas Backman <serenity@exscape.org>
2023-10-10 15:00:01 +0200

    Add simple Linux build script (Pyinstaller)

---
+++ buildscripts/pyinstaller_linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ ! -f "plex-hide-spoilers.py" ]]; then
+    echo "Build script is meant to be executed from the project root directory, e.g.: bash buildscripts/pyinstaller_linux.sh"
+    exit 1
+fi
+
+rm -rf dist build
+
+pyinstaller --contents-directory 'data' plex-hide-spoilers.py
+cp config_sample.toml README.md dist/plex-hide-spoilers
+cp -Rp licenses dist/plex-hide-spoilers
+
+cd dist
+
+echo "Creating .tar.bz2"
+tar cjf plex-hide-spoilers.tar.bz2 plex-hide-spoilers
